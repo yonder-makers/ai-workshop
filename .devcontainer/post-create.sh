bedce0d3c77a8929a411adf8f3f4167d662ece18
Calin C <calin.chiper@tss-yonder.com>
2023-10-26 00:48:18 +0300

    add pycache as volume

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,7 @@
 sudo chown vscode client/node_modules
 sudo chown vscode server/.env
 sudo chown vscode server/model
+sudo chown vscode server/__pycache__
 sudo chown vscode .cache
 
 # Install Python dependencies
