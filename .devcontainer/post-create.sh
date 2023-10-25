7832e2be18385187dba25b1bc19b519ada9bde0f
Calin C <calin.chiper@tss-yonder.com>
2023-10-26 00:42:26 +0300

    mount node_modules, .env, model and cache

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,15 @@
+sudo chown vscode client/node_modules
+sudo chown vscode server/.env
+sudo chown vscode server/model
+sudo chown vscode .cache
+
+# Install Python dependencies
+cd server
+python -m venv .env
+. .env/bin/activate
+pip install -r requirements.txt
+
+# Install Node dependencies
+cd ../client
+npm install
+
--- .devcontainer/setup.sh
+++
@@ -1,8 +0,0 @@
-
-# Install Python dependencies
-cd server
-pip3 install -r requirements.txt
-
-# Install Node dependencies
-cd ../client
-npm install
